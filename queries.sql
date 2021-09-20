a815aecec5fb92d6db71805d08cf88411fd1b23f
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2021-09-20 15:20:17 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,18 @@
-/*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+/*Matches animals that end in mon*/
+
+SELECT *
+FROM animals
+WHERE name ~ '[A-Za-z]+mon';
+
+/*Matches animals between 2016 and 2019*/
+SELECT *
+FROM animals
+WHERE date_of_birth > '1/1/2016' 
+AND date_of_birth < '1/1/2019';
+
+/*Matches animals neutered with less than 3 escapes*/
+SELECT *
+FROM animals
+WHERE neutered = TRUE
+AND escape_attempts < 3;
