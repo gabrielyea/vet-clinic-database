65bad6630d88e5987deabdfce9fbd7f8dd42897d
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2021-09-22 10:40:31 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -13,7 +13,7 @@ ALTER TABLE animals
     ADD COLUMN species VARCHAR(100);
 
 CREATE TABLE public.owners (
-	id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+    id int NOT NULL GENERATED ALWAYS AS IDENTITY,
     full_name varchar(100);
     age int;
 	CONSTRAINT owners_pk PRIMARY KEY (id)
