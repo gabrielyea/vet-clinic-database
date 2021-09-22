986a4902a4dcd7d8a1f682d2e87ab672feb9efdf
Gabriel Rendon <gabriel.gabrielyea@gmail.com>
2021-09-22 10:40:13 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -11,3 +11,10 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
     ADD COLUMN species VARCHAR(100);
+
+CREATE TABLE public.owners (
+	id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100);
+    age int;
+	CONSTRAINT owners_pk PRIMARY KEY (id)
+);
